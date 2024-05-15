d958b47739103dee79414eb202af34fe74e044b0
Christopher Daun <chrisdaun@gmail.com>
2024-05-14 21:19:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
-firmware_url = "https://raw.githubusercontent.com/chrisdaun/pico_ota"
+firmware_url = "https://raw.githubusercontent.com/chrisdaun/pico_ota/"
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 ota_updater.download_and_install_update_if_available()
 
